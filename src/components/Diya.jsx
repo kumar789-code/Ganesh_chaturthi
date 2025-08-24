f84610c92de0a5afe1cd53f036d961eb89d23098
kumar789-code <simran2003kushwaha@gmail.com>
2025-08-25 00:10:48 +0530

    fix: update diya path

--- src/components/Diya.jsx
+++ src/components/Diya.jsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
-import Diya from "/assets/diya.svg"
-
+import diya from "/assets/diya.svg"
 const Diya = () => {
   const [lit, setLit] = useState(false);
 
   return (
     <div className="diya" onClick={() => setLit(!lit)}>
-      <img src={Diya} alt="Diya" className="diya-base" />
+      <img src={diya} alt="Diya" className="diya-base" />
       {lit && <div className="flame"></div>}
     </div>
   );
